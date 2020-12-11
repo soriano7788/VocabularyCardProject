986160d22812d1116e3ec0e2c779219b94837937
Cano Shih <y781025@gmail.com>
2020-12-11 23:34:36 +0800

    update MSSQLDialect.sql, update CARD table

--- SQLScript/MSSQLDialect.sql
+++ SQLScript/MSSQLDialect.sql
@@ -11,3 +11,14 @@ create table CARD_SET (
 	STATE tinyint not null,
 	primary key (CARD_SET_ID)
 );
+create table CARD (
+	CARD_ID int identity not null,
+	FLAG int not null,
+	VOCABULARY nvarchar(255) not null,
+	CREATED_DATETIME datetime not null,
+	MODIFIED_DATETIME datetime not null,
+	CREATOR nvarchar(255) not null,
+	MODIFIER nvarchar(255) not null,
+	STATE tinyint not null,
+	primary key (CARD_ID)
+);
