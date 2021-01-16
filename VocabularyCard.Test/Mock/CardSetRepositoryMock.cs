e9e06fd6fde0190d696888c8abad86f4100ef670
Cano Shih <y781025@gmail.com>
2021-01-17 01:42:21 +0800

    先加個 VocabularyCard.Test 專案

---
+++ VocabularyCard.Test/Mock/CardSetRepositoryMock.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using VocabularyCard.Entities;
+using VocabularyCard.Repositories;
+
+namespace VocabularyCard.Test.Mock
+{
+    public class CardSetRepositoryMock : ICardSetRepository
+    {
+        public CardSet Create(CardSet entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IList<CardSet> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public CardSet GetByCardSetId(int cardSetId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IList<CardSet> GetByOwner(string ownerId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public CardSet Read(Expression<Func<CardSet, bool>> predicate)
+        {
+            throw new NotImplementedException();
+        }
+
+        public CardSet Update(CardSet cardSet)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ VocabularyCard.Test/TestCardSetService.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using VocabularyCard.Core;
+using VocabularyCard.Core.EF;
+using VocabularyCard.Dtos;
+using VocabularyCard.Repositories;
+using VocabularyCard.Repositories.EF;
+using VocabularyCard.Services;
+using VocabularyCard.Services.Impl;
+using VocabularyCard.AccountManager.DTO;
+
+namespace VocabularyCard.Test
+{
+    [TestFixture]
+    public class TestCardSetService
+    {
+        private ICardSetService _cardSetService;
+        private ICardSetRepository _cardSetRepository;
+        private ICardRepository _cardRepository;
+        IUnitOfWork _unitOfWork;
+
+        // 2.x 版時是 TestFixtureSetUp
+        [OneTimeSetUp]
+        public void TestCaseInit()
+        {
+            //DbContext context = new BaseDbContext();
+            //_unitOfWork = new EFUnitOfWork(context);
+            //_cardSetRepository = new EFCardSetRepository(context);
+            //_cardSetService = new CardSetService(_unitOfWork, _cardSetRepository, _cardRepository);
+            //Console.WriteLine("*************TestCaseInit************");
+        }
+
+        [Test]
+        public void Test_000_GetById(UserInfo user, int id)
+        {
+            //Arrange
+            UserInfo testUser = new UserInfo { UserId = "admin" };
+            int inputId = 1;
+
+            //Act
+            CardSetDto dto =  _cardSetService.GetById(testUser, inputId);
+            int actual = dto.Id;
+
+            //Assert
+            int expected = 1;
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
